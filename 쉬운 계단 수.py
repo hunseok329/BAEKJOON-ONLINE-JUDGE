30ee6cfcbe46dfff99f3e0d5f0e25be8dc37c87a
hunseok329 <kha5064@gmail.com>
2020-10-12 21:34:32 +0900

    #solve

---
+++ 쉬운 계단 수.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+dp = [[0 for x in range(10)] for y in range(num+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, num+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1])
+print(sum(dp[num])%1000000000)
