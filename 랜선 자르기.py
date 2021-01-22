6a1bfd2aec21d2a3bfb1c0c8a18e2abd00d28d44
hunseok329 <kha5064@gmail.com>
2021-01-22 14:41:59 +0900

    #solve

---
+++ 랜선 자르기.py
@@ -0,0 +1,21 @@
+K, N = map(int, input().split())
+
+line = []
+
+for _ in range(K):
+    line.append(int(input()))
+
+start, end = 1, max(line)
+
+while start <= end:
+    mid = (start+end)//2
+    
+    count = 0
+    for x in line:
+        count += x//mid
+        
+    if count >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
